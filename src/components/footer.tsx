3fff9168b42fecf63ed053df3d23ecdfe02359af
Christopher Peralta <cperalta@controlac.com>
2025-09-02 19:48:26 -0700

    removed phone number

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -157,14 +157,6 @@ export function Footer() {
               <i className="fa-regular fa-envelope text-blue-500" />{' '}
               contact.elevatewithg1
             </a>
-            <span className="hidden md:inline">|</span>
-            <a
-              href="tel:+19097062706"
-              className="flex items-center gap-1 hover:text-green-500 transition-colors cursor-pointer"
-              onClick={e => e.stopPropagation()}
-            >
-              <i className="fa-solid fa-phone text-green-500" /> (909) 706-2706
-            </a>
           </div>
         </div>
         <Typography
